726dbade6049e089e695be6976a1e5ad1f5a4c5e
ninyya <ebicninja@gmail.com>
2024-02-22 19:58:40 +0100

    Added .gitignore \& Thefuck

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,11 @@ if status is-interactive
 end
 
 fish_add_path "/home/nin/.local/bin"
+
+
+# Start or re-use a gpg-agent.
+gpgconf --launch gpg-agent
+
+# Ensure that GPG Agent is used as the SSH agent
+set -e SSH_AUTH_SOCK
+set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
