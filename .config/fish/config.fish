0ffa931d18d9059e90e374ec13dddc8121842fd0
ninyya <ebicninja@gmail.com>
2024-03-16 14:53:18 +0100

    Changed tmux theme, added temporary alias in fish for nvim-kick

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,6 @@ gpgconf --launch gpg-agent
 # Ensure that GPG Agent is used as the SSH agent
 set -e SSH_AUTH_SOCK
 set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+
+#Aliases
+alias nvim-kick='NVIM_APPNAME="nvim-kickstart" nvim'
